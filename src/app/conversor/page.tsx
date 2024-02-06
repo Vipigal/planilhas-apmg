5d8ae3ec0b7164bc7999da928b77e0308e7dba64
Vinicius Pinho <viniciuspgalvao@hotmail.com>
2024-02-05 21:27:31 -0300

    feat: primeira versao do corretor funcional

--- src/app/conversor/page.tsx
+++ src/app/conversor/page.tsx
@@ -18,22 +18,93 @@ export interface SpreadSheet {
 export default function Conversor() {
   const [isUploadLoading, setIsUploadLoading] = useState(false);
 
-  const converterArquivo = async (workbook: XLSX.WorkBook) => {
+  const isLetter = (char: string): boolean => {
+    return char.length === 1 && /[a-zA-Z]/.test(char);
+  };
+
+  const converterArquivo = async (
+    workbook: XLSX.WorkBook,
+    newFileName: string
+  ) => {
     const prepositions = ["da", "de", "do", "dos", "das", "e"];
     const permitidas = ["APM", "MG"];
     const sheetNames = workbook.SheetNames;
 
     const worksheet = workbook.Sheets[sheetNames[0]];
-    console.log(worksheet);
+    // const data = XLSX.utils.sheet_to_json(worksheet);
+    // Função para aplicar as regras de conversão em uma célula
+    const converterCelula = (cell: XLSX.CellObject): string => {
+      let value = cell.v?.toString().toLowerCase().trim();
+
+      // Verifica se a célula está vazia
+      if (!value) return "";
+
+      // Divide o valor em palavras
+      const words = value.split(/\s+/);
+
+      // Aplica as regras para cada palavra
+      const convertedWords = words.map((word, index) => {
+        // Verifica se a palavra é uma preposição em português
+        if (prepositions.includes(word)) {
+          return word;
+        }
+        // Verifica se a palavra é uma sigla permitida
+        else if (permitidas.includes(word.toUpperCase())) {
+          return word.toUpperCase();
+        }
+        // Mantém a primeira letra maiúscula para as demais palavras buscando comecar com uma letra sempre
+        else {
+          let indexLetra = 0;
+          while (
+            indexLetra < word.length &&
+            !isLetter(word.charAt(indexLetra))
+          ) {
+            indexLetra++;
+          }
+          if (indexLetra < word.length) {
+            return (
+              word.substring(0, indexLetra) +
+              word[indexLetra].toUpperCase() +
+              word.substring(indexLetra + 1)
+            );
+          }
+          return word.charAt(0).toUpperCase() + word.slice(1);
+        }
+      });
+
+      // Junta as palavras de volta em uma string
+      return convertedWords.join(" ");
+    };
+
+    // Percorre todas as células da planilha
+    for (const cellRange in worksheet) {
+      if (!cellRange.startsWith("!")) {
+        const cell = worksheet[cellRange];
+        // Verifica se a célula contém texto
+        if (cell.t === "s") {
+          // Converte o texto da célula
+          cell.v = converterCelula(cell);
+        }
+      }
+    }
+
+    // Salva o novo workbook em um arquivo
+    XLSX.writeFile(workbook, `${newFileName}.xlsx`);
   };
 
   const handleSubmitFiles = async (spreadsheets: SpreadSheet[]) => {
     setIsUploadLoading(true);
+    let index = 0;
     for (const spreadsheet of spreadsheets) {
       const file = spreadsheet.content;
       const fileBuffer = await file.arrayBuffer();
       const workbook = XLSX.read(fileBuffer);
-      await converterArquivo(workbook);
+      await converterArquivo(
+        workbook,
+        `${file.name.split(`.`)[0].trim().concat(`_convertido`)}` ||
+          `planilha-convertida-${index}`
+      );
+      index++;
     }
     //limpar array
     spreadsheets = [];
